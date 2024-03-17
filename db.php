bf9cd5e78a8762d89ddcce364bd7e0c627d2ee13
Aditi Bisoi <114893448+Aditi323@users.noreply.github.com>
2024-03-17 22:53:11 +0530

    first commit

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+
+//Your Mysql Config
+$servername = "localhost";
+$username = "root";
+$password="Aditi@011415";
+$dbname = "placement_portal";
+
+//Create New Database Connection
+$conn = new mysqli("localhost","root","Aditi@011415","placement_portal","3308");
+
+
